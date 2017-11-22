dbd21f1dce1c61c6b19282e55c284384b6849700
yokota <qykth-git@users.noreply.github.com>
2017-11-22 22:15:23 +0900

    Move JavaScript codes to its own files
    This makes to use editor's syntax highlight.

---
+++ docs/promo2qr-result.js
@@ -0,0 +1,11 @@
+      "use strict";
+      $(function() {
+        var url = location.search.substring(1);
+
+        var qr = qrcode(0, "L");
+        qr.addData(url);
+        qr.make();
+
+        $("div#qr-holder").html(qr.createSvgTag(5));
+        $("div#url"      ).text(url);
+      });
